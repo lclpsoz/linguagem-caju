06c093aa336087bc6c5f07928655e23e45c6f39b
Rafael Silva Menezes <rmenezes@questrade.com>
2024-10-11 15:49:01 -0300

    refactor: update java files to be more organized

---
+++ caju/utils/FileHandler.java
@@ -0,0 +1,32 @@
+package caju.utils;
+
+import caju.ast.ASTDisplay;
+import caju.ast.ASTPrinter;
+import caju.lexer.*;
+import caju.node.*;
+import caju.parser.*;
+
+import java.io.*;
+
+public class FileHandler {
+    
+    public static boolean isValidFile(String filePath) {
+        File file = new File(filePath);
+        return file.exists() && file.isFile() && file.canRead();
+    }
+
+    public static FileReader getFileReader(String filePath) throws IOException {
+        return new FileReader(new File(filePath));
+    }
+
+    public static void parseFile(String filePath) throws IOException, ParserException, LexerException {
+        try (FileReader fileReader = getFileReader(filePath)) {
+            Lexer lexer = new Lexer(new PushbackReader(fileReader, 1024));
+            Parser parser = new Parser(lexer);
+            Start tree = parser.parse();
+
+            tree.apply(new ASTPrinter());
+            tree.apply(new ASTDisplay());
+        }
+    }
+}
