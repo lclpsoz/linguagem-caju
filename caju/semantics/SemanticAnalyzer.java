a849e69617fff310b6a016181831092faf54da9e
Rafael Silva Menezes <rmenezes@questrade.com>
2024-10-20 19:51:53 -0300

    feat: implement some verify in semantic analyzer

--- caju/semantics/SemanticAnalyzer.java
+++ caju/semantics/SemanticAnalyzer.java
@@ -3,7 +3,14 @@ package caju.semantics;
 import java.util.Stack;
 
 import caju.analysis.DepthFirstAdapter;
-import caju.node.AAPrograma;
+import caju.node.ABlocoAComando;
+import caju.node.ABooleanoATipo;
+import caju.node.ACaractereATipo;
+import caju.node.ANumeroATipo;
+import caju.node.AVariavelADeclaracao;
+import caju.node.AVetorATipo;
+import caju.node.PANome;
+import caju.node.PATipo;
 import caju.node.Start;
 
 public class SemanticAnalyzer extends DepthFirstAdapter {
@@ -25,18 +32,47 @@ public class SemanticAnalyzer extends DepthFirstAdapter {
 
     public SemanticAnalyzer() {
         symbolTables = new Stack<>();
-        symbolTables.push(new SymbolTable(null)); // Global scope
+        symbolTables.push(new SymbolTable(null));
     }
 
-    public void inAPrograma(AAPrograma node) {
-        // Start of the program, global scope
-        symbolTables.push(new SymbolTable(symbolTables.peek())); // Push new scope
+    public void inABlocoAComando(ABlocoAComando node) {
+        symbolTables.push(new SymbolTable(symbolTables.peek()));
     }
 
-    public void outAPrograma(AAPrograma node) {
-        // End of the program, remove the global scope
+    @Override
+    public void outABlocoAComando(ABlocoAComando node) {
         symbolTables.pop();
     }
 
-    
+    @Override
+public void caseAVariavelADeclaracao(AVariavelADeclaracao node) {
+    Symbol.Type varType = getTypeFromNode(node.getATipo());
+
+    for (PANome nomeNode : node.getNomes()) {
+        String varName = nomeNode.toString();
+
+        SymbolTable currentScope = symbolTables.peek();
+        if (!currentScope.add(varName, new Symbol(varName, varType))) {
+            System.err.println("Erro: Variável '" + varName + "' já foi declarada no escopo atual.");
+        } else {
+            System.out.println("Variável '" + varName + "' declarada com sucesso.");
+        }
+    }
+}
+
+
+private Symbol.Type getTypeFromNode(PATipo tipoNode) {
+    if (tipoNode instanceof ANumeroATipo) {
+        return Symbol.Type.NUMERO;
+    } else if (tipoNode instanceof ACaractereATipo) {
+        return Symbol.Type.CARACTERE;
+    } else if (tipoNode instanceof ABooleanoATipo) {
+        return Symbol.Type.BOOLEANO;
+    } else if (tipoNode instanceof AVetorATipo) {
+        return Symbol.Type.VETOR;
+    } else {
+        return null; 
+    }
+}
+  
 }
